f053f28ef6a1562813d56cb82fc2860dcb0f0f88
Miter1 <60219780+Miter1@users.noreply.github.com>
2020-02-25 09:46:30 +0100

    My first website with some js

---
+++ pierwsza strona/skrypt.js
@@ -0,0 +1,6 @@
+document.getElementById('Guzik-i-link').addEventListener('click',function(){
+    let a = Math.round(Math.random() *10);
+    let b = Math.round(Math.random() *10);
+    let c = a+b;
+    alert(a + '+' + b + '=' + c);
+});
